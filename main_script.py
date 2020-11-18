55736e08c4bc76121810472a2b8ea7b5c254d03f
HarishaSN <harshas2509@gmail.com>
2020-11-18 09:46:11 +0530

    changed print statement to return statements

--- main_script.py
+++ main_script.py
@@ -6,11 +6,10 @@ print(input_array)
 def horizontal(a:list):
     for i in a:
         if(len(set(i))==1):
-            print(str(i[0])+" won")
+            return i[0]
             break
-    return
+    return -1
 
-horizontal(input_array)
                        
 def vertical(a:list):
     for i in range(3):
@@ -18,9 +17,26 @@ def vertical(a:list):
         for j in range(3):
             arr.append(a[j][i])
         if(len(set(arr))==1):
-            print(str(arr[0])+"won")
-            break
-    return
+            return arr[0]
+    return -1
 
-vertical(input_array)
+
+def diagonal(a:list):
+    arr=list()
+    count=2
+    arr.append([])
+    for i in range(3):
+        arr[-1].append(a[i][i])
+    arr.append([])
+    for j in range(3):
+        arr[-1].append(a[j][count])
+        count-=1
+    for i in arr:
+        if(len(set(i))==1):
+            return i[0]
+    return -1
+
+
+
+        
     
