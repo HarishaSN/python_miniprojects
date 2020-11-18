e2dab4a914bc2175f182a3a88e63bb41f35441fc
HarishaSN <harshas2509@gmail.com>
2020-11-18 09:14:04 +0530

    added horizontal function

--- main_script.py
+++ main_script.py
@@ -2,6 +2,15 @@ input_array=list()
 for i in range(3):
     input_array.append(list(map(int,input().split(" "))))
 print(input_array)
+
+def horizontal(a:list):
+    for i in a:
+        if(len(set(i))==1):
+            print(str(i[0])+" won")
+            break
+    return
+
+horizontal(input_array)
                        
 
     
